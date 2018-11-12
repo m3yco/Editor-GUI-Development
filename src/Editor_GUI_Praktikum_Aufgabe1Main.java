2a740c2eb8da37e73efbba636a4f817e10a578d3
m3yco <milazzdo@web.de>
2018-11-12 11:44:57 +0100

    v3

--- src/Editor_GUI_Praktikum_Aufgabe1Main.java
+++
@@ -1,9 +0,0 @@
-public class Editor_GUI_Praktikum_Aufgabe1Main {
-
-	public static void main(String[] args) {
-		Editor_GUI_Praktikum_Aufgabe1 gui = new Editor_GUI_Praktikum_Aufgabe1();
-		gui.open();
-
-	}
-
-}
